2f5e9bc62c2c994fbf02cf5556ef08741c80f579
Zane Little-James <zane1092@gmail.com>
2021-06-22 18:15:48 +1200

    Add project files.

---
+++ Breakout/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Breakout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(324, 432);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(105, 18);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(547, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lives: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Location = new System.Drawing.Point(264, 290);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(10, 10);
+            this.ball.TabIndex = 73;
+            this.ball.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(728, 450);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.downPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.upPress);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox ball;
+    }
+}
+
---
+++ Breakout/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout
+{
+    public partial class Form1 : Form
+    {
+        int blocksx = 10, blocksy = 5;
+        int blockw = 50, blockh = 20;
+        int blocksSpace = 2;
+        PictureBox[,] blocks = new PictureBox[5, 10];
+        //PictureBox[,] blocks;
+
+        int lives = 5;
+
+        bool goleft;
+        bool goright;
+        bool running;
+
+        int ballx = 5;
+        int bally = 5;
+
+        int walk = 5;
+        int runM = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Shown += CreateBlocksDelegate;
+        }
+
+        private void CreateBlocksDelegate(object sender, EventArgs e)
+        {
+            //PictureBox[,] blocks = new PictureBox[blocksy, blocksx];
+            for (int j = 0; j < blocksy; j++)
+            {
+                for (int i = 0; i < blocksx; i++)
+                {
+                    blocks[j, i] = new PictureBox();
+                    blocks[j, i].Size = new Size(blockw, blockh);
+                    blocks[j, i].Location = new Point((blockw + blocksSpace) * i, ((blockh + blocksSpace) * j) + 50);
+                    blocks[j, i].BackColor = Color.Red;
+                    blocks[j, i].SetBounds(blocks[j,i].Location.X, blocks[j, i].Location.Y, blockw, blockh);
+                    //blocks[j, i].SetBounds((blockw + blocksSpace) * i, ((blockh + blocksSpace) * j) + 50, blockw, blockh);
+
+                    this.Controls.Add(blocks[j, i]);
+                }
+            }
+
+            this.Size = new Size((blockw + blocksSpace) * blocksx, 500);
+        }
+
+        private void downPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+
+            if (e.Modifiers == Keys.Shift)
+            {
+                running = true;
+            }
+        }
+
+        private void upPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+
+            if (e.Modifiers == Keys.Shift)
+            {
+                running = false;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            ball.Top -= bally;
+            ball.Left -= ballx;
+
+            if (ball.Left < 0 || ball.Left + ball.Width > ClientSize.Width)
+            {
+                ballx = -ballx;
+            }
+
+            if (ball.Top < 0)
+            {
+                bally = -bally;
+            }
+
+            if (ball.Bounds.IntersectsWith(player.Bounds))
+            {
+                bally = -bally;
+            }
+
+            if(ball.Bottom > ClientSize.Height)
+            {
+                ball.Top = ClientSize.Height / 2+100;
+                ball.Left = ClientSize.Width / 2;
+                bally = -bally;
+            }
+
+            for (int j = 0; j < blocksy; j++)
+            {
+                for (int i = 0; i < blocksx; i++)
+                {
+                    if (ball.Bounds.IntersectsWith(blocks[j,i].Bounds))
+                    {
+                        blocks[j,i].SetBounds(0,0,0,0);
+                        this.Controls.Remove(blocks[j, i]);
+
+                        bally = -bally;
+                    }
+                }
+            }
+            
+
+
+            if (running)
+            {
+                if (goleft == true && player.Left > 0)
+                {
+                    player.Left -= walk * runM;
+                }
+
+                if (goright == true && player.Left + player.Width < ClientSize.Width)
+                {
+                    player.Left += walk * runM;
+                }
+            }
+            else
+            {
+                if (goleft == true && player.Left > 0)
+                {
+                    player.Left -= walk;
+                }
+
+                if (goright == true && player.Left + player.Width < ClientSize.Width)
+                {
+                    player.Left += walk;
+                }
+            }
+        }
+    }
+}
